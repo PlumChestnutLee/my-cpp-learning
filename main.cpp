a006b3099f0607323fdad283dae3ad9fdc778cea
PlumChestnutLee <jinkuli123@163.com>
2017-04-16 20:06:19 +0800

    add main

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by James Li on 2017/4/16.
+//
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int y =15;
+    int *ip;
+    ip = &y;
+    cout << ip << &y << y << *ip;
+}
+
